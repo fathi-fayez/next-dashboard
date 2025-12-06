01b9c9aaefbc2290fe8c12607fc8be3e99bcb442
fathi-fayez <fathifayez155@gmail.com>
2025-12-06 21:37:37 +0200

    add pagination to customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,26 @@
-export default function Page() {
-    return (
-        <main className="flex min-h-screen flex-col p-6">
-            <p>Customers Page</p>
-        </main>
-    );
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers, fetchCustomersPages } from '@/app/lib/data';
+import Pagination from '@/app/ui/invoices/pagination';
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchCustomersPages(query);
+  const customers = await fetchFilteredCustomers(query, currentPage);
+
+  return (
+    <div className="w-full">
+      <CustomersTable customers={customers} />
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
